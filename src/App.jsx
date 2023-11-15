be03449392084c16ee3c1a817973321174ab5d1c
lichtle <leticia.negreiros@outlook.com>
2023-11-15 18:58:05 -0300

    chore: finalização da validação (falta aprimorar)

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,37 @@
 import { useState } from "react";
 
-import Formulario from "./components/Formulario";
 import Perfil from "./components/Perfil";
 import ReposList from "./components/ReposList";
 
 function App() {
-  const [formularioEstaVisivel, setFormularioEstaVisivel] = useState(true);
+  const [nomeUsuario, setNomeUsuario] = useState(""); // Criando um useState para determinar que nomeUsuario deve corresponder ao valor inserido pelo usuário no input abaixo
+
+  const handleSubmitFormulario = (event) => {
+    event.preventDefault();
+    setNomeUsuario(event.target.elements.user.value);
+  };
 
   return (
     <div className="App">
-      <Perfil nomeUsuario="lichtle" />
-      <ReposList nomeUsuario="lichtle" />
-      {/* {formularioEstaVisivel && <Formulario />}
-      <button
-        type="button"
-        onClick={() => {
-          setFormularioEstaVisivel(!formularioEstaVisivel); // Criando a função de alternância (toggle) da visibilidade do componente "Formulário"
-        }}
-      >
-        Toggle formulário
-      </button> */}
+      <form className="form-container" onSubmit={handleSubmitFormulario}>
+        <label className="label" htmlFor="user">Busque por um nome de usuário:</label>
+        <div className="search">
+          <input
+            className="input"
+            type="text"
+            id="user"
+            // onBlur={(event) => setNomeUsuario(event.target.value)} // Quando o evento onBlur (preenchimento do input seguido de clique fora do campo) ocorre, dispara o evento setNomeUsuario, função que determina que nomeUsuario deve corresponder ao valor inserido pelo usuário no input
+          />
+          <button className="btn-search" type="submit">Buscar</button>
+        </div>
+      </form>
+      {nomeUsuario.length >= 1 && ( // Condicionando a renderização dos repositórios somente quando um usuário for inserido no input
+        <>
+          <Perfil nomeUsuario={nomeUsuario} />
+          {/* Determinando que a prop nomeUsuario (utilizada nos componentes filhos dos componentes Perfil e ReposList) deve corresponder à nomeUsuario. Acima, nomeUsuario já foi definido como sendo o valor inserido pelo cliente no input */}
+          <ReposList nomeUsuario={nomeUsuario} />
+        </>
+      )}
     </div>
   );
 }
--- src/components/ReposList/index.jsx
+++ src/components/ReposList/index.jsx
@@ -4,43 +4,53 @@ import styles from "./ReposList.module.css";
 
 const ReposList = ({ nomeUsuario }) => {
   const [repos, setRepos] = useState([]);
+  const [estaCarregando, setEstaCarregando] = useState(false);
 
   useEffect(() => {
     async function getUser() {
+      setEstaCarregando(true);
       const requisicao = await fetch(
         `https://api.github.com/users/${nomeUsuario}/repos`
       );
       const respostaEmJson = await requisicao.json();
-      
+
+      respostaEmJson.message === "Not Found"
+        ? alert("Usuário não encontrado! :(")
+        : setEstaCarregando(false);
+
       setRepos(respostaEmJson);
     }
 
     getUser();
-  }, []); // Se o fetch for mal sucedido podemos adicionar "nomeUsuario" dentro deste array vazio para informar o useEffect que ele deve executar a função getUser quando houver alguma mudança na prop nomeUsuario
+  }, [nomeUsuario]); // Podemos adicionar a prop "nomeUsuario" dentro deste array vazio para informar o useEffect que ele deve executar a função de fetch getUser toda vez que houver alguma mudança na prop nomeUsuario
 
   return (
     <div className="container">
-      <ul className={styles.list}>
-        {repos.map(
-          (
-            { id, name, language, html_url } // O parâmetro da função anônima retorna um objeto, e podemos utilizar a desestruturação para ter acesso apenas aos itens desejados, eliminando a necessidade de repetir "objeto.propriedade" várias vezes
-          ) => (
-            <li key={id} className={styles.listItem}>
-              <div className={styles.itemName}>
-                <span>Nome:</span>
-                {name}
-              </div>
-              <div className={styles.itemLanguage}>
-                <span>Linguagem:</span>
-                {language}
-              </div>
-              <a href={html_url} target="_blank" className={styles.itemLink}>
-                Visitar no GitHub
-              </a>
-            </li>
-          )
-        )}
-      </ul>
+      {estaCarregando ? (
+        <h1 className={styles.loading}>Carregando repositórios...</h1>
+      ) : (
+        <ul className={styles.list}>
+          {repos.map(
+            (
+              { id, name, language, html_url } // O parâmetro da função anônima retorna um objeto, e podemos utilizar a desestruturação para ter acesso apenas aos itens desejados, eliminando a necessidade de repetir "objeto.propriedade" várias vezes
+            ) => (
+              <li key={id} className={styles.listItem}>
+                <div className={styles.itemName}>
+                  <span>Nome:</span>
+                  {name}
+                </div>
+                <div className={styles.itemLanguage}>
+                  <span>Linguagem:</span>
+                  {language}
+                </div>
+                <a href={html_url} target="_blank" className={styles.itemLink}>
+                  Visitar no GitHub
+                </a>
+              </li>
+            )
+          )}
+        </ul>
+      )}
     </div>
   );
 };
